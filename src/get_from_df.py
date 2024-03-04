e30b29032bb8447c25f1ab14c806734c2af293a1
Antonio Pardo de Santayana <201903588@alu.comillas.edu>
2024-03-04 22:12:40 +0100

    added more patterns, up to batch 4 (n=111)

--- src/get_from_df.py
+++ src/get_from_df.py
@@ -11,7 +11,7 @@ import os
 import sys
 import re
 
-NUM_BATCHES = 1
+NUM_BATCHES = 5
 
 # Some companies report in millions, some in thousands. We ignore share count for now and get the multiplier for each company
 def assign_multiplier(text):
@@ -25,13 +25,18 @@ def clean_value(value):
     try:
         return float(re.sub(r"[^\d\.,-]", "", value))
     except ValueError:
-        return None
+        return 0
+    except TypeError:
+        return 0
 
 def find_metric(ws, fields, multiplier):
     for row in ws.iter_rows(values_only=True):
         if row[0] and any(substring in row[0].lower() for substring in fields):
             value = row[1]
-            print(f'name found is {row[0]}, value is {row[1]}')
+            if row[1] is None or 'other' in row[0].lower():
+                continue
+            if row[0].lower() == 'basic' and clean_value(row[1]) > 1000:
+                continue
             if isinstance(value, str):
                 value = clean_value(value)
             try:
@@ -50,26 +55,34 @@ def process_file(file_path, patterns):
         company={'Name': ws['A2'].value, 'Ticker': ws['A3'].value}
         multiplier = assign_multiplier(ws['A9'].value.lower())
         for name, fields in patterns.items():
-            print(f'For name {name}:')
             if name == 'EPS':
                 company[name] = find_metric(ws, fields, 1)
             else:
                 company[name] = find_metric(ws, fields, multiplier)
-                # if name == 'Liabilities and Equity':
-                #     company['Total Liabilities'] = company['Liabilities and Equity'] - company['Equity']
-                #     del company['Liabilities and Equity']
+                if name == 'Liabilities and Equity':
+                    try:
+                        company['Total Liabilities'] = company['Liabilities and Equity'] - company['Equity']
+                        del company['Liabilities and Equity']
+                    except:
+                        pass
         companies.append(company)
     with open('test.json', 'a') as f:
         json.dump(companies, f, indent=2)
+    return companies
 
 
 def main():
-    df = pd.DataFrame()
+    full_data = []
+    print("Last modified time:", os.path.getmtime('patterns.json'))
     with open('patterns.json', 'r') as f:
         patterns = json.load(f)
     for i in range(NUM_BATCHES):
         file_path = f'data/batch_{i}.xlsx'
-        process_file(file_path, patterns)
+        companies = process_file(file_path, patterns)
+        full_data.extend(companies)
+    df = pd.DataFrame(full_data)
+    print(df.head())
+    df.to_excel('full_data.xlsx', index=False)
         
 
 if __name__ == '__main__':
